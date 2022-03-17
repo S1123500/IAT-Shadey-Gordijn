63603d7b6cea0312350c3387b84cca901ec64fcc
David <davidfraterman@gmail.com>
2022-03-17 20:01:38 +0100

    open close slider fixje

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -15,7 +15,7 @@
         <section class="openCloseSlider__container">
             <p>Close</p>
             <section class="openCloseSlider">
-                <input type="range" class="openCloseSlider__slider" id="openCloseSlider__slider" min="0" value="0" max="3" step="1" list="openCloseSlider__options">
+                <input type="range" class="openCloseSlider__slider" id="openCloseSlider__slider" min="0" value="0" max="4" step="1" list="openCloseSlider__options">
             </section>
             <p>Open</p>
         </section>
