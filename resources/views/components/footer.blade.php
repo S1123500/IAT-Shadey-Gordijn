bb2c1604bb6e1d2823bd023bdf9c29b9b811d961
evavandongen <96053258+evavandongen@users.noreply.github.com>
2022-03-23 15:36:49 +0100

    a taggies toevoegen naar homepage

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,12 +2,14 @@
     <nav>
         <ul>
             <li><span class="material-icons u-noselect">bedtime</span></li>
-            <li>
-                {{-- Add active class if current path is '/' --}}
-                <span class="{{ Request::is('/') ? 'u-active' : '' }} material-icons u-noselect">
-                    home
-                </span>
-            </li>
+            <a href="{{'/'}}">
+                <li>
+                    {{-- Add active class if current path is '/' --}}
+                    <span class="{{ Request::is('/') ? 'u-active' : '' }} material-icons u-noselect">
+                        home
+                    </span>
+                </li>
+            </a>
             <li><span class="material-icons u-noselect">settings</span></li>
         </ul>
     </nav>
